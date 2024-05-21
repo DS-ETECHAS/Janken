3254df738e31cc7319a3cbde61be916f5fa65a26
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-05-20 22:38:02 -0300

    initial project

---
+++ PedraPapelETesoura/Models/Jogador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedraPapelETesoura.Models
+{
+    public sealed class Jogador
+    {
+        public string Nome { get; set; }
+        public Opcao Escolha { get; set; }
+        public int Pontuação { get; set; }
+    }
+}
---
+++ PedraPapelETesoura/Views/JoKenPoView.xaml.cs
@@ -0,0 +1,9 @@
+namespace PedraPapelETesoura.Views;
+
+public partial class JoKenPoView : ContentPage
+{
+	public JoKenPoView()
+	{
+		InitializeComponent();
+	}
+}
